a177c8ab54ec7b5b2a24152b2642cfaffabff3ec
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2024-11-10 23:05:29 +0100

    added accordion

--- plugins/section/src/components.js
+++ plugins/section/src/components.js
@@ -1,6 +1,117 @@
 export default (editor, opts = {}) => {
   const domc = editor.DomComponents;
 
+  // accordion
+  domc.addType('B-ACCORDION', {
+    model: {
+      defaults: {
+        tagName: 'div',
+        attributes: { id: 'accordionExample', class: 'container col-xxl-8 px-4 py-5 accordion' },
+        components: [
+          {
+            tagName: 'h2',
+            type: 'text',
+            attributes: { class: 'pb-2 border-bottom' },
+            components: [{
+              type: 'textnode',
+              content: 'Accordion'
+            }]
+          },
+          {
+            tagName: 'div',
+            attributes: { class: 'accordion-item' },
+            components: [
+              {
+                tagName: 'h2',
+                type: 'text',
+                attributes: { class: 'accordion-header', id: 'headingOne' },
+                components: [{
+                  tagName: 'button',
+                  attributes: { class: 'accordion-button', type: 'button', 'data-bs-toggle': "collapse", 'data-bs-target': "#collapseOne", 'aria-expanded': "true", 'aria-controls': "collapseOne" },
+                  components: [{
+                    type: 'textnode',
+                    content: 'Accordion Item #1'
+                  }]
+                }]
+              },
+              {
+                tagName: 'div',
+                attributes: { class: 'accordion-collapse collapse show', id: 'collapseOne', 'aria-labelledby': "headingOne", 'data-bs-parent': "#accordionExample" },
+                components: [
+                  {
+                    tagName: 'div',
+                    type: 'text',
+                    attributes: { class: 'accordion-body' },
+                    components: [
+                      {
+                        tagName: 'strong',
+                        components: [{
+                          type: 'textnode',
+                          content: `This is the first item's accordion body.`
+                        }]
+                      },
+                      {
+                        type: 'textnode',
+                        content: `It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the accordion-body, though the transition does limit overflow`
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          },
+          {
+            tagName: 'div',
+            attributes: { class: 'accordion-item' },
+            components: [
+              {
+                tagName: 'h2',
+                type: 'text',
+                attributes: { class: 'accordion-header', id: 'headingTwo' },
+                components: [{
+                  tagName: 'button',
+                  attributes: { class: 'accordion-button', type: 'button', 'data-bs-toggle': "collapse", 'data-bs-target': "#collapseTwo", 'aria-expanded': "true", 'aria-controls': "collapseTwo" },
+                  components: [{
+                    type: 'textnode',
+                    content: 'Accordion Item #1'
+                  }]
+                }]
+              },
+              {
+                tagName: 'div',
+                attributes: { class: 'accordion-collapse collapse show', id: 'collapseTwo', 'aria-labelledby': "headingTwo", 'data-bs-parent': "#accordionExample" },
+                components: [
+                  {
+                    tagName: 'div',
+                    type: 'text',
+                    attributes: { class: 'accordion-body' },
+                    components: [
+                      {
+                        tagName: 'strong',
+                        components: [{
+                          type: 'textnode',
+                          content: `This is the second item's accordion body.`
+                        }]
+                      },
+                      {
+                        type: 'textnode',
+                        content: `It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the accordion-body, though the transition does limit overflow`
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+    },
+
+    view: {
+
+    },
+  });
+
   domc.addType('NAVBAR', {
     model: {
       defaults: {
