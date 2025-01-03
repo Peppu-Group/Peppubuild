57ca13273f5b6828c419bf827d878dfa46e628af
Ukpai Ugochi <ukpaiugochiibem@gmail.com>
2025-01-03 17:13:20 +0100

    removed local test

--- plugins/sidebar/src/locale/en.js
+++ plugins/sidebar/src/locale/en.js
@@ -9,7 +9,7 @@ export default {
             placeholder: 'project name',
             publish: 'Publish Project +',
             load: 'Load Project +',
-            url: 'http://localhost:1404',
+            url: 'https://server.peppubuild.com',
         },
         settings: {
             save: 'Save',
